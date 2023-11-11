4f6879997814d7ecd2a9a914ccd4e738993a8c9d
Caleb <calebwagner5@gmail.com>
2023-11-11 10:37:01 -0600

    added HTTPClient to recieve responses from api

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,6 +1,7 @@
 package com.pluralsight.courseinfo.cli;
 
 
+import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,5 +25,9 @@ public class CourseRetriever {
 
     private static void retrieveCourses(String authorId) {
         LOG.info("Retrieving courses for author '{}'", authorId);
+        CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
+
+        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
+        LOG.info("Retrieved the following courses {}", coursesToStore);
     }
 }
