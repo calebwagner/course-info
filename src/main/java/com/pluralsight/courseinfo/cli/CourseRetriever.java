50840b49995de2c2a65ddeb7b565d395f7a71426
Caleb <calebwagner5@gmail.com>
2023-11-11 09:41:44 -0600

    built basic cli tool to run locally

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -4,5 +4,21 @@ public class CourseRetriever {
 
     public static void main(String... args) {
         System.out.println("course info started ...");
+
+        if (args.length == 0) {
+            System.out.println("Please provide an author name as a first argument.");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            System.out.println("Unexpected error");
+            e.printStackTrace();
+        }
+    }
+
+    private static void retrieveCourses(String authorId) {
+        System.out.println("Retrieving courses for author " + authorId);
     }
 }
