006c2c97b82aef057a8b3129e57af14ca28d653e
Caleb <calebwagner5@gmail.com>
2023-11-18 09:27:46 -0600

    unified logs and added external app configs for db

--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -5,17 +5,41 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
+import java.util.logging.LogManager;
 
 public class CourseServer {
+
+    // ensure calls to j.u.logging API are redirected to SLF4J
+    static {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.install();
+    }
+
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
     private static final String BASE_URL = "http://localhost:8080/";
     public static void main(String... args) {
-        LOG.info("Starting HTTP server");
+        String databaseFileName = loadDatabaseFilename();
+        LOG.info("Starting HTTP server with database {}", databaseFileName);
         CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
         ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
 
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URL), config);
     }
+
+    private static String loadDatabaseFilename() {
+        try (InputStream propertiesStream = CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+        return properties.getProperty("course-info.database");
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Could not load database filename");
+        }
+    }
+
 }
